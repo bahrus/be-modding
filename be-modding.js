7614a9b3d7f80d2816938a1870620e391eae9aa9
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-22 06:37:29 -0500

    Initial commit

--- be-modding.js
+++ be-modding.js
@@ -8,6 +8,7 @@ import { dispatchEvent as de } from 'trans-render/positractions/dispatchEvent.js
 
 /**
  * @implements {Actions}
+ * @implements {EventListenerObject}
  */
 class BeModding extends BE {
     de = de;
@@ -30,7 +31,7 @@ class BeModding extends BE {
      * @param {BAP} self 
      */
     async processJS(self){
-        const {jsExpr} = self;
+        const {jsExpr, enhancedElement} = self;
         const eventHandlerClauses = jsExpr.split(reClause);
         const {splitOnce} = await import('trans-render/lib/splitOnce.js');
         const guid = `a_${crypto.randomUUID()}`;
@@ -40,7 +41,7 @@ class BeModding extends BE {
             const eventMerge = splitOnce(clause, ':');
             scriptText += `${eventMerge[0]}: e => {
                 const {h} = e;
-                return ${eventMerge[1]}
+                e.r = ${eventMerge[1]}
             } ,`;
             console.log({eventMerge});
         }
@@ -51,6 +52,10 @@ class BeModding extends BE {
         console.log({scriptText});
         document.head.appendChild(script);
         const handlers = script[guid];
+        for(const event in handlers){
+            enhancedElement.addEventListener(event, this);
+            enhancedElement.addEventListener(event, handlers[event]);
+        }
         // console.log({jsExpr, eventHandlerClauses});
         
         console.log({handlers});
@@ -58,9 +63,61 @@ class BeModding extends BE {
             resolved: true,
         });
     }
+
+    /**
+     * 
+     * @param {Event} e 
+     */
+    async handleEvent(e){
+        if(e instanceof EnhancedEvent){
+            return;
+        }
+        const {type, target} = e;
+        if(target instanceof HTMLElement){
+            
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            /**
+             * @type {Element | null | undefined}
+             */
+            let h = target.closest('[itemscope]');
+            if(h){
+                h = await (await import('trans-render/dss/getHostish.js')).getHostish(h);
+            }
+            if(!h){
+                h = /** @type {any | undefined} */ (target.getRootNode()).host;
+            }
+            if(!h) throw 404;
+            console.log({h});
+            const evt = new EnhancedEvent(type, h);
+            target.dispatchEvent(evt);
+            const {r} = evt;
+            if(r){
+                Object.assign(h, r);
+            }
+        }
+
+    }
 }
 
 await BeModding.bootUp();
 export {BeModding}
 
 export const reClause = /(?<!\?)\,(?![^{}]*\})/;
+
+export class EnhancedEvent extends Event{
+    /**
+     * @type {EventTarget}
+     */
+    h;
+    r;
+    /**
+     * 
+     * @param {string} type 
+     * @param {EventTarget} h 
+     */
+    constructor(type, h){
+        super(type);
+        this.h = h;
+    }
+}
